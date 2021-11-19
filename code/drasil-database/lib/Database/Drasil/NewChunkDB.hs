a640c6107ad5ddf7240f904c02a5b6766d469082
Jason Balaci <jasonbalaci@gmail.com>
2021-11-19 15:08:40 -0500

    ChunkDB unions!

--- code/drasil-database/lib/Database/Drasil/NewChunkDB.hs
+++ code/drasil-database/lib/Database/Drasil/NewChunkDB.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE TypeApplications#-}
+
 module Database.Drasil.NewChunkDB (
       ChunkDB
     , mkChunkDB
     , find, findOrErr
     , findAll
     , insert
+    , union
     , registered
 ) where
 
@@ -13,12 +16,25 @@ import Database.Drasil.UID (UID, HasUID(..))
 import qualified Data.Map as M
 import Data.List (nub)
 import Data.Maybe (fromMaybe, mapMaybe)
-import Data.Typeable (Typeable, typeOf, TypeRep)
+import Data.Typeable
 
 type ChunkByUID      = M.Map UID Chunk
 type ChunksByTypeRep = M.Map TypeRep [Chunk]
 
 newtype ChunkDB = ChunkDB (ChunkByUID, ChunksByTypeRep)
+
+{-
+TODO: Discuss important design decisions. These will become rather critical to Drasil projects that rely on ChunkDBs.
+
+1. UID uniqueness and immutability after placement
+2. `union`
+3. No `ChunkDB`s inside of `ChunkDB`s!
+
+
+TODO: Near-future:
+1. Should all Chunks, on insertion, be checked to ensure that the knowledge they depend on already exists in the ChunkDB?
+
+-}
 
 find :: Typeable a => UID -> ChunkDB -> Maybe a
 find u (ChunkDB tc) = do
@@ -36,7 +52,10 @@ findAll tr (ChunkDB (_, trm)) = maybe [] (mapMaybe unChunk) (M.lookup tr trm)
 
 insert :: (HasUID a, Typeable a) => ChunkDB -> a -> ChunkDB
 insert (ChunkDB (cu, ctr)) c
-    | M.member (uid c) cu = error $ "Attempting to register a chunk which already contains a UID; `" ++ show (uid c) ++ "`"
+    | typeRep cu == typeRep (Proxy @ChunkDB) = 
+        error "Insertion of ChunkDBs in ChunkDBs is disallowed; please perform unions with them instead."
+    | M.member (uid c) cu =
+        error $ "Attempting to register a chunk which already contains a UID; `" ++ show (uid c) ++ "`"
     | otherwise           = ChunkDB (cu', ctr')
         where
             c' :: Chunk
@@ -47,6 +66,16 @@ insert (ChunkDB (cu, ctr)) c
 
             ctr' :: ChunksByTypeRep
             ctr' = M.alter (Just . maybe [c'] (++ [c'])) (typeOf c) ctr
+
+union :: ChunkDB -> ChunkDB -> ChunkDB
+union (ChunkDB (lum, ltrm)) (ChunkDB (rum, rtrm)) = ChunkDB (um, trm)
+    where
+        um :: ChunkByUID
+        um = M.unionWith (\conflict _ -> error $ "Unioned ChunkDBs contains at least one UID collision; `" ++ show (uid conflict) ++ "`!") lum rum
+        
+        -- NOTE: This is an important design decision, we are joining with preference for assuming that the LEFT comes first (in other words, is the "earlier" discovered information)
+        trm :: ChunksByTypeRep
+        trm = M.unionWith (++) ltrm rtrm
 
 mkChunkDB :: [Chunk] -> ChunkDB
 mkChunkDB cs = ChunkDB (cbu, csbtr)
