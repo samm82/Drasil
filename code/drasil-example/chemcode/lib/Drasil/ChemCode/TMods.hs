07427839cb4675cb233e75b96a9b788169493236
Samuel Crawford <sam.crawford82@gmail.com>
2023-04-14 17:58:43 -0400

    add Law of Conservation of Mass TM (without correct equation)

--- code/drasil-example/chemcode/lib/Drasil/ChemCode/TMods.hs
+++ code/drasil-example/chemcode/lib/Drasil/ChemCode/TMods.hs
@@ -5,14 +5,15 @@ import qualified Data.List.NonEmpty as NE
 import Language.Drasil
 import Theory.Drasil
 
-import Data.Drasil.Citations (ilpWiki)
+import Data.Drasil.Citations (ilpWiki, lund2023)
+import Data.Drasil.Concepts.Chemistry (chemical, reaction)
 import Data.Drasil.Concepts.Math (equation)
 import Data.Drasil.Concepts.Software (program)
 
 import Drasil.ChemCode.Quantities (aMat, bVec, cVec, xVec, zeroVec)
 
 tms :: [TheoryModel]
-tms = [intLinProg]
+tms = [intLinProg, lawConsMass]
 
 intLinProg :: TheoryModel
 intLinProg = tm
@@ -42,3 +43,27 @@ intLinProg = tm
   where
     ilpChunk =
       dccWDS "ilpChunk" (nounPhraseSP "integer linear program") (S "") -- FIXME: ?
+
+lawConsMass :: TheoryModel
+lawConsMass = tm
+  (ilpModel "lawConsMass" (cn' "law of conservation of mass")
+    $ maxILP xVec cVec
+      $ sy aMat `mulRe` sy xVec $<= sy bVec NE.:| [sy xVec $>= sy zeroVec, isIn (sy xVec) (Vect Integer)])
+  ([] :: [QuantityDict]) -- FIXME: I should not need to manually define the type signature for this to type check
+  [consMassChunk] -- FIXME: why do I need this?
+  []
+  [sy cVec `mulRe` sy xVec,
+   sy aMat `mulRe` sy xVec $<= sy bVec,
+   sy xVec $>= sy zeroVec,
+   isIn (sy xVec) (Vect Integer)] -- FIXME: apparently this is needed since generation doesn't happen from ModelKinds
+  []
+  [dRef lund2023]
+  "lawConsMass" -- FIXME: this is likely needed for the same ModelKinds reason
+  [foldlSent [S "This law states that", Quote (foldlSent_ [
+      S "matter can neither be created nor destroyed in a", phrase chemical,
+      phrase reaction, S "... but it may change forms to other substances"]),
+    complexRef lund2023 (Page [112])]
+  ]
+  where
+    consMassChunk =
+      dccWDS "consMassChunk" (nounPhraseSP "law of conservation of mass") (S "") -- FIXME: ?
