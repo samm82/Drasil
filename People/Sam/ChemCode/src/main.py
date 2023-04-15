d6009e6373c917b0d5fc863d1abe17e1e4d46da8
Samuel Crawford <sam.crawford82@gmail.com>
2023-04-14 22:58:13 -0400

    connect parse functionality to main

--- People/Sam/ChemCode/src/main.py
+++ People/Sam/ChemCode/src/main.py
@@ -1,8 +1,13 @@
 from convertMatrix import convert
 from elementClass import E
+from parseEqn import parse
 from solveILP import solve
 
 def main():
+	# modified from https://www.wikihow.com/Balance-Chemical-Equations-Using-Linear-Algebra
+	# to follow assumptions of ChemCode
+	eq = "H3PO4 + N2H8MoO4 + HNO3 -> N3H12PO40Mo12 + N2H4O3 + H2O"
+
 	c1 = [(E.H, 3), (E.P, 1), (E.O, 4)]
 	c2 = [(E.N, 2), (E.H, 8), (E.Mo, 1), (E.O, 4)]
 	c3 = [(E.H, 1), (E.N, 1), (E.O, 3)]
@@ -12,8 +17,10 @@ def main():
 
 	r = ([c1, c2, c3], [c4, c5, c6])
 
+	assert parse(eq) == r
+
 	# mat rep of chemical equation
-	A = convert(r)
+	A = convert(parse(eq))
 	# A = [[3, 8, 1, -12, -4, -2], [1, 0, 0, -1, 0, 0], [4, 4, 3, -40, -3, -1], [0, 2, 1, -3, -2, 0], [0, 1, 0, -12, 0, 0]]
 
 	res = solve(A)
--- People/Sam/ChemCode/src/parseEqn.py
+++ People/Sam/ChemCode/src/parseEqn.py
@@ -3,14 +3,21 @@ from regex import sub
 from elementClass import E
 
 def parse(eq):
+	out = []
 	eq = eq.split(" -> ")
 	for i in [0, 1]:
-		eq[i] = eq[i].split(" + ")
-		for j in range(len(eq[i])):
+		side = []
+		sideRaw = eq[i].split(" + ")
+		for j in range(len(sideRaw)):
+			c = []
 			# split letters and numbers
-			eq[i][j] = sub(r"([A-Z][a-z]?)([0-9]+)", r"\g<1>-\g<2>-", eq[i][j])
+			cRaw = sub(r"([A-Z][a-z]?)([0-9]+)", r"\g<1>-\g<2>-", sideRaw[j])
 			# split elements with no implicit subscript (and add it)
-			eq[i][j] = sub(r"([A-Z]|[a-z])(?=[A-Z]|$)", r"\g<1>-1-", eq[i][j])
-	print(eq)
-
-parse("H3PO4 + N2H8MoO4 + HNO3 -> N3H12PO40Mo12 + N2H4O3 + H2O")
+			cRaw = sub(r"([A-Z]|[a-z])(?=[A-Z]|$)", r"\g<1>-1-", cRaw)
+			# split on added delimiter, removing empty strings
+			cRaw = [x for x in cRaw.split('-') if x]
+			for k in range(int(len(cRaw) / 2)):
+				c.append((E[cRaw[2*k]], int(cRaw[2*k + 1])))
+			side.append(c)
+		out.append(side)
+	return tuple(out)
