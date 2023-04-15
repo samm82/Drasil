e370d05bb73d11a0e03758ef2faefc39b8d936ed
Samuel Crawford <sam.crawford82@gmail.com>
2023-04-14 21:35:38 -0400

    define count and convert functions

--- People/Sam/ChemCode/src/convertMatrix.py
+++ People/Sam/ChemCode/src/convertMatrix.py
@@ -17,6 +17,24 @@ def elems(r):
 				out.add(e[0])
 	return out
 
-print(elems(r))
+def count(e, c):
+	for c_i in c:
+		if c_i[0] == e:
+			return c_i[1]
+	return 0
+
+def convert(r):
+	out = []
+	for e in elems(r):
+		row = []
+		for c in r[0]:
+			row.append(count(e, c))
+		for c in r[1]:
+			row.append(-count(e, c))
+		out.append(row)
+	return out
+
+print(convert(r))
+
 
 A = [[3, 8, 1, -12, -4, -2], [1, 0, 0, -1, 0, 0], [4, 4, 3, -40, -3, -1], [0, 2, 1, -3, -2, 0], [0, 1, 0, -12, 0, 0]]
