e82450046aafe1696366f99aca74270c91b3f872
Samuel Crawford <sam.crawford82@gmail.com>
2023-04-17 22:54:57 -0400

    remove goal statement regarding output, since other Drasil examples do not include these

--- code/drasil-example/chemcode/lib/Drasil/ChemCode/Goals.hs
+++ code/drasil-example/chemcode/lib/Drasil/ChemCode/Goals.hs
@@ -2,17 +2,17 @@ module Drasil.ChemCode.Goals (goals) where
 
 import Language.Drasil
 
-import Data.Drasil.Concepts.Documentation (goalStmtDom, output_)
+import Data.Drasil.Concepts.Documentation (goalStmtDom)
 
 goals :: [ConceptInstance]
-goals = [findBalancedForm, outputInfeasMsg]
+goals = [findBalancedForm] --, outputInfeasMsg]
 
 findBalancedForm :: ConceptInstance
 findBalancedForm = cic "findBalancedForm" (S "Find its balanced" +:+.
     S "form with the smallest positive integer coefficients if it is feasible")
   "findBalancedForm" goalStmtDom
 
-outputInfeasMsg :: ConceptInstance
-outputInfeasMsg = cic "outputInfeasMsg" (atStart output_ +:+.
-    S "a descriptive message if it is infeasible")
-  "outputInfeasMsg" goalStmtDom
+-- outputInfeasMsg :: ConceptInstance
+-- outputInfeasMsg = cic "outputInfeasMsg" (atStart output_ +:+.
+--     S "a descriptive message if it is infeasible")
+--   "outputInfeasMsg" goalStmtDom
