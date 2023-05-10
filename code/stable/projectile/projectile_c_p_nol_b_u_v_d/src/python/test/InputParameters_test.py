d4e31591a8da348800e00da100009922ea4e543a
Samuel Crawford <sam.crawford82@gmail.com>
2023-05-10 19:59:44 -0400

    parameterized tests

--- code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/InputParameters_test.py
+++ code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/InputParameters_test.py
@@ -4,6 +4,7 @@
 
 from math import isclose
 from pathlib import Path
+from pytest import mark
 from sys import path
 path.append("../")
 from python import InputParameters
@@ -12,27 +13,29 @@ from python import InputParameters
 from contextlib import redirect_stdout
 from io import StringIO
 
+valid_input_files = ["default_float", "default_int"]
+
 def read_inParams(filename):
     return InputParameters.InputParameters(Path("test/test_input") / f"{filename}.txt")
 
 # \brief Tests reading valid input
-def test_get_input_valid():
-    for filename in ["default_float", "default_int"]:
-        inParams = read_inParams(filename)
+@mark.parametrize("filename", valid_input_files)
+def test_get_input_valid(filename):
+    inParams = read_inParams(filename)
 
-        assert isclose(inParams.v_launch, 20)
-        if filename.endswith("float"):
-            assert isclose(inParams.theta, 0.785398)
-        elif filename.endswith("int"):
-            assert isclose(inParams.theta, 1)
-        assert isclose(inParams.p_target, 41)
+    assert isclose(inParams.v_launch, 20)
+    if filename.endswith("float"):
+        assert isclose(inParams.theta, 0.785398)
+    elif filename.endswith("int"):
+        assert isclose(inParams.theta, 1)
+    assert isclose(inParams.p_target, 41)
 
 # \brief Tests constraint checking valid input
-def test_input_constraints_valid():
-    for filename in ["default_float", "default_int"]:
-        inParams = read_inParams(filename)
+@mark.parametrize("filename", valid_input_files)
+def test_input_constraints_valid(filename):
+    inParams = read_inParams(filename)
 
-        stdout = StringIO()
-        with redirect_stdout(stdout):  
-            inParams.input_constraints()
-        assert stdout.getvalue() == ""
+    stdout = StringIO()
+    with redirect_stdout(stdout):  
+        inParams.input_constraints()
+    assert stdout.getvalue() == ""
