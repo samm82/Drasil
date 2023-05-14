582e1f3349159e52d1c1180bf0c802400b8386de
Samuel Crawford <sam.crawford82@gmail.com>
2023-05-14 17:08:43 -0400

    add tests for p_land with valid input

--- code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
+++ code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
@@ -16,10 +16,12 @@ expected_valid_input_calculations = [
     {
         "filename": "default_float",
         "t_flight": 2.8861496557,
+        "p_land":   40.816326531,
     },
     {
         "filename": "default_int",
         "t_flight": 3.4345754482,
+        "p_land":   37.114180687,
     },
 ]
 
@@ -32,3 +34,9 @@ def get_expected(field):
 def test_func_t_flight(filename, t_flight):
     inParams = read_inParams(filename)
     assert isclose(Calculations.func_t_flight(inParams, g), t_flight)
+
+# \brief Tests calculation of p_land with valid input
+@mark.parametrize("filename,p_land", get_expected("p_land"))
+def test_func_p_land(filename, p_land):
+    inParams = read_inParams(filename)
+    assert isclose(Calculations.func_p_land(inParams, g), p_land)
