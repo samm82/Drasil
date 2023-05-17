8565147b295a327a17d23c601957988a6506cce4
Samuel Crawford <sam.crawford82@gmail.com>
2023-05-17 18:56:07 -0400

    reuse input data for consistency

--- code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/OutputFormat_test.py
+++ code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/OutputFormat_test.py
@@ -5,16 +5,10 @@
 from pytest import mark
 
 from python import OutputFormat
-
-expected_outputs = [
-    # (s, d_offset, t_flight)
-    ("The target was hit.",        -0.183673469, 2.8861496557), # result of default_float
-    ("The projectile fell short.", -3.885819313, 3.4345754482), # result of default_int
-    ("The projectile went long.",  0.816326531,  2.8861496557), # result of projectile_went_long
-]
+from .TestHelpers import get_expected
 
 # \brief Tests writing valid input
-@mark.parametrize("s, d_offset, t_flight", expected_outputs)
+@mark.parametrize("s, d_offset, t_flight", get_expected("d_offset", "t_flight"))
 def test_get_input_valid(s, d_offset, t_flight):
     OutputFormat.write_output(s, d_offset, t_flight)
     with open("output.txt") as f:
