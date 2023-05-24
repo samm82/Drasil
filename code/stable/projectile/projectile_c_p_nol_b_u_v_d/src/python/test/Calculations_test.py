2b9e3b8bfd0d3dcf106e6282814a7d6706d54f4f
Samuel Crawford <sam.crawford82@gmail.com>
2023-05-24 14:26:50 -0400

    add tests for negative value of g for Calculations module

--- code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
+++ code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
@@ -9,29 +9,50 @@ from python import Calculations
 from .TestHelpers import get_expected, read_inParams
 
 valid_g = 9.8
+neg_g = -9.8
 epsilon = 0.02
 
 # \brief Tests calculation of t_flight with valid input
 @mark.parametrize("filename,t_flight", get_expected("t_flight"))
-def test_valid_func_t_flight(filename, t_flight):
+def test_func_t_flight_valid(filename, t_flight):
     inParams = read_inParams(filename)
     assert isclose(Calculations.func_t_flight(inParams, valid_g), t_flight)
 
 # \brief Tests calculation of p_land with valid input
 @mark.parametrize("filename,p_land", get_expected("p_land"))
-def test_valid_func_p_land(filename, p_land):
+def test_func_p_land_valid(filename, p_land):
     inParams = read_inParams(filename)
     assert isclose(Calculations.func_p_land(inParams, valid_g), p_land)
 
 # \brief Tests calculation of d_offset with valid input
 @mark.parametrize("filename,d_offset", get_expected("d_offset"))
-def test_valid_func_d_offset(filename, d_offset):
+def test_func_d_offset_valid(filename, d_offset):
     inParams = read_inParams(filename)
     assert isclose(Calculations.func_d_offset(
         inParams, Calculations.func_p_land(inParams, valid_g)), d_offset)
     
 # \brief Tests calculation of s with valid input
 @mark.parametrize("filename,d_offset,s", get_expected("d_offset", "s"))
-def test_valid_func_s(filename, d_offset, s):
+def test_neg_func_s_valid(filename, d_offset, s):
     inParams = read_inParams(filename)
     assert Calculations.func_s(inParams, epsilon, d_offset) == s
+
+# TODO: should the following tests also be made into Control tests?
+# \brief Tests calculation of t_flight with negative gravitational acceleration
+@mark.parametrize("filename,t_flight", get_expected("t_flight"))
+def test_func_t_flight_neg_g(filename, t_flight):
+    inParams = read_inParams(filename)
+    assert isclose(Calculations.func_t_flight(inParams, neg_g), -t_flight)
+
+# \brief Tests calculation of p_land with negative gravitational acceleration
+@mark.parametrize("filename,p_land", get_expected("p_land"))
+def test_func_p_land_neg_g(filename, p_land):
+    inParams = read_inParams(filename)
+    assert isclose(Calculations.func_p_land(inParams, neg_g), -p_land)
+
+# \brief Tests calculation of d_offset with negative gravitational acceleration
+@mark.parametrize("filename,d_offset,p_target", get_expected("d_offset", "p_target"))
+def test_func_d_offset_neg_g(filename, d_offset,p_target):
+    inParams = read_inParams(filename)
+    assert isclose(Calculations.func_d_offset(
+        inParams, Calculations.func_p_land(inParams, neg_g)), -d_offset - 2 * p_target)
