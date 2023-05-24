580ff134ccb31ff228dda8d3cedfd7ed485aca1e
Samuel Crawford <sam.crawford82@gmail.com>
2023-05-24 14:21:06 -0400

    specify that a value of 9.8 is a valid value of g

--- code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
+++ code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
@@ -8,30 +8,30 @@ from pytest import mark
 from python import Calculations
 from .TestHelpers import get_expected, read_inParams
 
-g = 9.8
+valid_g = 9.8
 epsilon = 0.02
 
 # \brief Tests calculation of t_flight with valid input
 @mark.parametrize("filename,t_flight", get_expected("t_flight"))
-def test_func_t_flight(filename, t_flight):
+def test_valid_func_t_flight(filename, t_flight):
     inParams = read_inParams(filename)
-    assert isclose(Calculations.func_t_flight(inParams, g), t_flight)
+    assert isclose(Calculations.func_t_flight(inParams, valid_g), t_flight)
 
 # \brief Tests calculation of p_land with valid input
 @mark.parametrize("filename,p_land", get_expected("p_land"))
-def test_func_p_land(filename, p_land):
+def test_valid_func_p_land(filename, p_land):
     inParams = read_inParams(filename)
-    assert isclose(Calculations.func_p_land(inParams, g), p_land)
+    assert isclose(Calculations.func_p_land(inParams, valid_g), p_land)
 
 # \brief Tests calculation of d_offset with valid input
 @mark.parametrize("filename,d_offset", get_expected("d_offset"))
-def test_func_d_offset(filename, d_offset):
+def test_valid_func_d_offset(filename, d_offset):
     inParams = read_inParams(filename)
     assert isclose(Calculations.func_d_offset(
-        inParams, Calculations.func_p_land(inParams, g)), d_offset)
+        inParams, Calculations.func_p_land(inParams, valid_g)), d_offset)
     
 # \brief Tests calculation of s with valid input
 @mark.parametrize("filename,d_offset,s", get_expected("d_offset", "s"))
-def test_func_s(filename, d_offset, s):
+def test_valid_func_s(filename, d_offset, s):
     inParams = read_inParams(filename)
     assert Calculations.func_s(inParams, epsilon, d_offset) == s
