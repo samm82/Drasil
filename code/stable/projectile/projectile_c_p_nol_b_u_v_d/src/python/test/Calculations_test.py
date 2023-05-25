753fc017d462c133fa129ac66e2aaed210d8e9b0
Samuel Crawford <sam.crawford82@gmail.com>
2023-05-24 21:03:16 -0400

    reduce line length in Calculations_test

--- code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
+++ code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
@@ -17,18 +17,21 @@ epsilon = 0.02
 # \brief Tests calculation of t_flight with valid input
 @mark.parametrize("filename,t_flight", get_expected("t_flight"))
 def test_func_t_flight_valid(filename, t_flight):
-    assert isclose(Calculations.func_t_flight(conftest.inParams[filename], valid_g), t_flight)
+    assert isclose(Calculations.func_t_flight(conftest.inParams[filename], valid_g),
+                       t_flight)
 
 # \brief Tests calculation of p_land with valid input
 @mark.parametrize("filename,p_land", get_expected("p_land"))
 def test_func_p_land_valid(filename, p_land):
-    assert isclose(Calculations.func_p_land(conftest.inParams[filename], valid_g), p_land)
+    assert isclose(Calculations.func_p_land(conftest.inParams[filename], valid_g),
+                       p_land)
 
 # \brief Tests calculation of d_offset with valid input
 @mark.parametrize("filename,d_offset", get_expected("d_offset"))
 def test_func_d_offset_valid(filename, d_offset):
     assert isclose(Calculations.func_d_offset(
-        conftest.inParams[filename], Calculations.func_p_land(conftest.inParams[filename], valid_g)), d_offset)
+        conftest.inParams[filename], Calculations.func_p_land(
+            conftest.inParams[filename], valid_g)), d_offset)
     
 # \brief Tests calculation of s with valid input
 @mark.parametrize("filename,d_offset,s", get_expected("d_offset", "s"))
@@ -39,18 +42,21 @@ def test_neg_func_s_valid(filename, d_offset, s):
 # \brief Tests calculation of t_flight with negative gravitational acceleration
 @mark.parametrize("filename,t_flight", get_expected("t_flight"))
 def test_func_t_flight_neg_g(filename, t_flight):
-    assert isclose(Calculations.func_t_flight(conftest.inParams[filename], neg_g), -t_flight)
+    assert isclose(Calculations.func_t_flight(conftest.inParams[filename], neg_g),
+                       -t_flight)
 
 # \brief Tests calculation of p_land with negative gravitational acceleration
 @mark.parametrize("filename,p_land", get_expected("p_land"))
 def test_func_p_land_neg_g(filename, p_land):
-    assert isclose(Calculations.func_p_land(conftest.inParams[filename], neg_g), -p_land)
+    assert isclose(Calculations.func_p_land(conftest.inParams[filename], neg_g),
+                       -p_land)
 
 # \brief Tests calculation of d_offset with negative gravitational acceleration
 @mark.parametrize("filename,d_offset,p_target", get_expected("d_offset", "p_target"))
 def test_func_d_offset_neg_g(filename, d_offset,p_target):
     assert isclose(Calculations.func_d_offset(
-        conftest.inParams[filename], Calculations.func_p_land(conftest.inParams[filename], neg_g)), -d_offset - 2 * p_target)
+        conftest.inParams[filename], Calculations.func_p_land(
+            conftest.inParams[filename], neg_g)), -d_offset - 2 * p_target)
 
 # \brief Tests calculation of t_flight with zero gravitational acceleration
 @mark.parametrize("filename", get_expected())
