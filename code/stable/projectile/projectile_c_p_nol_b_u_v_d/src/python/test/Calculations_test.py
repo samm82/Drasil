0c8bb4b50e127f7b56926611938e53751417fc8c
Samuel Crawford <sam.crawford82@gmail.com>
2023-05-27 17:19:22 -0400

    remove tests for Calculations module that DO NOT use mocks

--- code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
+++ code/stable/projectile/projectile_c_p_nol_b_u_v_d/src/python/test/Calculations_test.py
@@ -4,9 +4,9 @@
 
 from math import isclose
 from pytest import mark, raises
+from unittest.mock import Mock
 
 from python import Calculations
-from . import conftest
 from .TestHelpers import get_expected, read_inParams
 
 valid_g = 9.8
@@ -14,58 +14,68 @@ neg_g = -9.8
 zero_g = 0
 epsilon = 0.02
 
+def build_mocks(*attrs):
+    mocks = []
+    defaults = ["v_launch", "theta", "p_target"]
+    for d in get_expected(*(defaults + list(attrs))):
+        mock_attrs = dict()
+        for i, attr in enumerate(defaults + list(attrs), start=1):
+            mock_attrs[attr] = d[i]
+        mock = Mock()
+        mock.configure_mock(**mock_attrs)
+        mocks.append(mock)
+    return mocks
+
 ## \brief Tests calculation of t_flight with valid input
-@mark.parametrize("filename,t_flight", get_expected("t_flight"))
-def test_func_t_flight_valid(filename, t_flight):
-    assert isclose(Calculations.func_t_flight(conftest.inParams[filename], valid_g),
-                       t_flight)
+@mark.parametrize("mock", build_mocks("t_flight"))
+def test_func_t_flight_valid(mock):
+    assert isclose(Calculations.func_t_flight(mock, valid_g), mock.t_flight)
 
 ## \brief Tests calculation of p_land with valid input
-@mark.parametrize("filename,p_land", get_expected("p_land"))
-def test_func_p_land_valid(filename, p_land):
-    assert isclose(Calculations.func_p_land(conftest.inParams[filename], valid_g),
-                       p_land)
+@mark.parametrize("mock", build_mocks("p_land"))
+def test_func_p_land_valid(mock):
+    assert isclose(Calculations.func_p_land(mock, valid_g), mock.p_land)
 
 ## \brief Tests calculation of d_offset with valid input
-@mark.parametrize("filename,d_offset", get_expected("d_offset"))
-def test_func_d_offset_valid(filename, d_offset):
-    assert isclose(Calculations.func_d_offset(
-        conftest.inParams[filename], Calculations.func_p_land(
-            conftest.inParams[filename], valid_g)), d_offset)
+@mark.parametrize("mock", build_mocks("p_land", "d_offset"))
+def test_func_d_offset_valid(mock):
+    assert isclose(Calculations.func_d_offset(mock, mock.p_land), mock.d_offset)
     
 ## \brief Tests calculation of s with valid input
-@mark.parametrize("filename,d_offset,s", get_expected("d_offset", "s"))
-def test_neg_func_s_valid(filename, d_offset, s):
-    assert Calculations.func_s(conftest.inParams[filename], epsilon, d_offset) == s
+@mark.parametrize("mock", build_mocks("d_offset", "s"))
+def test_func_s_valid(mock):
+    assert Calculations.func_s(mock, epsilon, mock.d_offset) == mock.s
 
 # TODO: should the following tests also be made into Control tests?
-## \brief Tests calculation of t_flight with negative gravitational acceleration
-@mark.parametrize("filename,t_flight", get_expected("t_flight"))
-def test_func_t_flight_neg_g(filename, t_flight):
-    assert isclose(Calculations.func_t_flight(conftest.inParams[filename], neg_g),
-                       -t_flight)
+## \brief Tests calculation of t_flight with negative gravitational acceleration \n
+#  and valid input
+@mark.parametrize("mock", build_mocks("t_flight"))
+def test_func_t_flight_neg_g(mock):
+    assert isclose(Calculations.func_t_flight(mock, neg_g), -mock.t_flight)
 
-## \brief Tests calculation of p_land with negative gravitational acceleration
-@mark.parametrize("filename,p_land", get_expected("p_land"))
-def test_func_p_land_neg_g(filename, p_land):
-    assert isclose(Calculations.func_p_land(conftest.inParams[filename], neg_g),
-                       -p_land)
+## \brief Tests calculation of p_land with negative gravitational acceleration \n
+#  and valid input
+@mark.parametrize("mock", build_mocks("p_land"))
+def test_func_p_land_neg_g(mock):
+    assert isclose(Calculations.func_p_land(mock, neg_g), -mock.p_land)
 
-## \brief Tests calculation of d_offset with negative gravitational acceleration
-@mark.parametrize("filename,d_offset,p_target", get_expected("d_offset", "p_target"))
-def test_func_d_offset_neg_g(filename, d_offset,p_target):
-    assert isclose(Calculations.func_d_offset(
-        conftest.inParams[filename], Calculations.func_p_land(
-            conftest.inParams[filename], neg_g)), -d_offset - 2 * p_target)
+## \brief Tests calculation of d_offset with negative gravitational acceleration \n
+#  and valid input
+@mark.parametrize("mock", build_mocks("p_land", "d_offset", "p_target"))
+def test_func_d_offset_neg_g(mock):
+    assert isclose(Calculations.func_d_offset(mock, -mock.p_land),
+                     -mock.d_offset - 2 * mock.p_target)
 
-## \brief Tests calculation of t_flight with zero gravitational acceleration
-@mark.parametrize("filename", get_expected())
-def test_func_t_flight_zero_g(filename):
+## \brief Tests calculation of t_flight with zero gravitational acceleration \n
+#  and valid input
+@mark.parametrize("mock", build_mocks())
+def test_func_t_flight_zero_g(mock):
     with raises(ZeroDivisionError):
-        Calculations.func_t_flight(conftest.inParams[filename], zero_g)
+        Calculations.func_t_flight(mock, zero_g)
 
-## \brief Tests calculation of p_land with zero gravitational acceleration
-@mark.parametrize("filename", get_expected())
-def test_func_p_land_zero_g(filename):
+## \brief Tests calculation of p_land with zero gravitational acceleration \n
+#  and valid input
+@mark.parametrize("mock", build_mocks())
+def test_func_p_land_zero_g(mock):
     with raises(ZeroDivisionError):
-        Calculations.func_p_land(conftest.inParams[filename], zero_g)
+        Calculations.func_p_land(mock, zero_g)
