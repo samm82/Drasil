0e06deb726e18e1a17fe6ee8e4af1d13fcaa9039
Samuel Crawford <sam.crawford82@gmail.com>
2024-06-28 17:41:51 -0400

    Remove mulRe from chemCode (see JacquesCarette/Drasil#3812)

--- code/drasil-example/chemcode/lib/Drasil/ChemCode/TMods.hs
+++ code/drasil-example/chemcode/lib/Drasil/ChemCode/TMods.hs
@@ -1,48 +1,55 @@
 module Drasil.ChemCode.TMods where
 
-import qualified Data.List.NonEmpty as NE
-
-import Language.Drasil
-import Theory.Drasil
-
 import Data.Drasil.Citations (ilpWiki)
 import Data.Drasil.Concepts.Math (equation)
 import Data.Drasil.Concepts.Software (program)
-
+import qualified Data.List.NonEmpty as NE
 import Drasil.ChemCode.Quantities (aMat, bVec, cVec, xVec, zeroVec)
+import Language.Drasil
+import Theory.Drasil
 
 tms :: [TheoryModel]
 tms = [intLinProg]
 
 intLinProg :: TheoryModel
-intLinProg = tm
-  (equationalConstraints' ilpCS)
-  ([] :: [QuantityDict]) -- FIXME: I should not need to manually define the type signature for this to type-check.
-  [ilpChunk] -- FIXME: Why do I need this?
-  []
-  [ilpRel] -- FIXME: I should not need to manually reference ilpRel twice.
-  []
-  [dRef ilpWiki]
-  "intLinProg"
-  [foldlSent [S "The above", phrase equation, -- FIXME: can this be called an "equation"?
-    S "gives the canonical form of an integer linear",
-    phrase program `sC` S "which is", Quote (foldlSent_ [
-      S "a mathematical optimization or feasibility", phrase program,
-      S "in which some or all of the variables are restricted to be integers",
-      S "[and] the objective function and the constraints", 
-      sParen (S "other than the integer constraints"), S "are linear"]),
-    refS ilpWiki],
-  S "The values of" +:+ ch xVec +:+. S "are unknown and will be solved for"
-  ]
+intLinProg =
+  tm
+    (equationalConstraints' ilpCS)
+    ([] :: [QuantityDict]) -- FIXME: I should not need to manually define the type signature for this to type-check.
+    [ilpChunk] -- FIXME: Why do I need this?
+    []
+    [ilpRel] -- FIXME: I should not need to manually reference ilpRel twice.
+    []
+    [dRef ilpWiki]
+    "intLinProg"
+    [ foldlSent
+        [ S "The above",
+          phrase equation, -- FIXME: can this be called an "equation"?
+          S "gives the canonical form of an integer linear",
+          phrase program `sC` S "which is",
+          Quote
+            ( foldlSent_
+                [ S "a mathematical optimization or feasibility",
+                  phrase program,
+                  S "in which some or all of the variables are restricted to be integers",
+                  S "[and] the objective function and the constraints",
+                  sParen (S "other than the integer constraints"),
+                  S "are linear"
+                ]
+            ),
+          refS ilpWiki
+        ],
+      S "The values of" +:+ ch xVec +:+. S "are unknown and will be solved for"
+    ]
   where
     ilpChunk =
       dccWDS "ilpChunk" (nounPhraseSP "integer linear program") (S "") -- FIXME: ?
-
     ilpRel :: ModelExpr
-    ilpRel = completeCase [
-      (sy cVec `mulRe` sy xVec, lit $ int 1),
-      (sy aMat `mulRe` sy xVec $<= sy bVec, lit $ int 2),
-      (sy xVec $>= sy zeroVec, lit $ int 3)
-      ]
+    ilpRel =
+      completeCase
+        [ (sy cVec $* sy xVec, lit $ int 1),
+          (sy aMat $* sy xVec $<= sy bVec, lit $ int 2),
+          (sy xVec $>= sy zeroVec, lit $ int 3)
+        ]
 
     ilpCS = mkConstraintSet ilpChunk $ NE.fromList [ilpRel]
